eb302998eb968c2f52af7fa66c73e5ee8103fcef
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2021-12-24 13:58:38 +0530

    init'

---
+++ src/pages/counter/counter.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useState } from "react";
+import Button from "../../components/Button/button";
+import styles from "./counter.module.css";
+
+const Counter = (props) => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementHandler = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
+  const decrementHandler = () => {
+    setCounter((prevState) => prevState - 1);
+  };
+
+  return (
+    <Fragment>
+      <h1>Counter</h1>
+      <div className={styles.counter}>
+        <Button onClick={decrementHandler}>-</Button>
+        <h2>{counter}</h2>
+        <Button onClick={incrementHandler}>+</Button>
+      </div>
+    </Fragment>
+  );
+};
+export default Counter;
